47598400aade6378194c87c116ceef99952667fc
Jonatha da Silva Pedrozas <158524181+Jpzin32@users.noreply.github.com>
2024-07-03 16:08:15 -0300

    Add files via upload

--- Jonatha -Python/apostila.py
+++ Jonatha -Python/apostila.py
@@ -1,7 +1,62 @@
-nome = ('jp','yan','matheus','will')
-carros = ['EB112', 'LaFerrari','civic','civicTypeR']
+#tuple = ('jp','yan','matheus','will')
+#list = ['EB112', 'LaFerrari','civic','civicTypeR']
+#dicionario = {'chave': 'valor'}
+#dicionario = {'nome': 'carro', 'lógica': 'true', 'numero': '2', 'outroNumero': '3.5'}
+#print(dicionario)
+#conjunto = {'carro' ,True, 2, 3.5}
+#print(conjunto)
 
-print (nome[0])
-print(nome[1])
-print(carros[2])
-print(carros[3])
+
+
+#print (tuple[0])
+#print(tuple[1])
+#print(list[2])
+#print(list[3])
+
+
+
+#carros = ['EB112', 'LaFerrari','civic','civicTypeR']
+#for carros in carros: #VAI COLOCAR CADA PALAVRA SEPARADA UMA EMBAIXO DA OUTRA
+#print(carros)
+
+
+
+#CONVERTENDO LIST EM TUPLE
+#List = [1,2,3,4,5]
+#print(List[1])
+#Tuple = tuple(List)
+#print(Tuple[3])
+
+
+
+#LIST
+#alunos =['jp', ' will', ' yan', 'vitor','teteu']
+#alunos.sort()#SORTEIA A ORDEM DA LISTA
+#alunos.pop(1)#REMOVE O VALOR SELECIONADO
+#alunos.append('jp3')#ADICIONANDO UM NOVO ELEMENTO
+#alunos.insert(1, 'Jp2')#ADICIONA O VALOR ESCRITO NO LUGAR DETERMINADO
+#print(alunos)
+
+
+
+#DICIONARIO
+#Jonatha = {
+#   'nome': 'Jonatha',
+#   'idade': 16,
+#    'altura': 1.70,
+#    'peso': 60,
+#    'cor': 'pardo'
+#}
+#Jonatha ['peso']= '58,97' #MUDA A CHAVE SELECIONADA
+#print(Jonatha['nome'], Jonatha['idade'], Jonatha['peso'])
+#del Jonatha['cor']#DELETAR UMA CHAVE DO DICIONARIO
+#print(len(Jonatha))#VER A QUANTIDADE DE CHAVE DENTRO DO DICIONARIO
+#print('altura' in Jonatha)#VERIFICAR SE ESSA CHAVE EXISTE DENTRO DO DICIONARIO
+#for x in Jonatha:
+ #print(x) #LER AS CHAVES DENTRO DO DICIONARIO (TEM QUE TER UM ESPAÇO DO PRINT ENTRE A BORDA DA PAG)
+#for x in Jonatha:
+    #print(x+': '+Jonatha[x])#MOSTRA A CHAVE E VALOR
+#print(Jonatha.get('sobrenome', 'essa info n conta'))#LEVA UMA INFORMAÇÃO AO USUARIO SE A CHAVE EXISTE OU NÃO
+#print(Jonatha.get('status','essa info n conta'))
+#Jonatha.clear()#RETIRA TUDO DE DENTRO DO DICIONARIO
+#print(Jonatha)
